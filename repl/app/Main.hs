f296eed59b30fcc3c862c710a5353fb69ba9087e
David Bernado <dbernado@protonmail.com>
2021-01-15 18:07:21 -0800

    update Main

--- repl/app/Main.hs
+++ repl/app/Main.hs
@@ -2,7 +2,7 @@ module Main where
 
 import Parser (readExpr)
 import Evaluator (eval)
-import Types (trapError, extractValue)
+import Types (Env, trapError, extractValue, liftResult, nullEnv, runIOResult)
 
 import Control.Monad
 import System.Environment
@@ -14,11 +14,11 @@ flushStr str = putStr str >> hFlush stdout
 readPrompt :: String -> IO String
 readPrompt prompt = flushStr prompt >> getLine
 
-evalString :: String -> IO String
-evalString expr = return $ extractValue $ trapError (liftM show $ readExpr expr >>= eval)
+evalString :: Env -> String -> IO String
+evalString env expr = runIOResult $ liftM show $ (liftResult $ readExpr expr) >>= eval env
 
-evalAndPrint :: String -> IO ()
-evalAndPrint expr =  evalString expr >>= putStrLn
+evalAndPrint :: Env -> String -> IO ()
+evalAndPrint env expr = evalString env expr >>= putStrLn
 
 until_ :: Monad m => (a -> Bool) -> m a -> (a -> m ()) -> m ()
 until_ pred prompt action = do 
@@ -27,13 +27,16 @@ until_ pred prompt action = do
       then return ()
       else action result >> until_ pred prompt action
 
+runOne :: String -> IO ()
+runOne expr = nullEnv >>= flip evalAndPrint expr
+
 runRepl :: IO ()
-runRepl = until_ (== "quit") (readPrompt "Future>>> ") evalAndPrint
+runRepl = nullEnv >>= until_ (== "quit") (readPrompt "Future>>> ") . evalAndPrint
 
 main :: IO ()
 main = do
   args <- getArgs
   case length args of
     0 -> runRepl
-    1 -> evalAndPrint $ args !! 0
+    1 -> runOne $ args !! 0
     _ -> putStrLn "Program takes only 0 or 1 arguments"
