b5f7a17b9ecd231d059f210f4f5aff205261d9ce
David Bernado <dbernado@protonmail.com>
2021-02-28 16:13:08 -0800

    add bool, char and empty list

--- src/compiler.scm
+++ src/compiler.scm
@@ -1,11 +1,50 @@
 (load "test-driver.scm")
 (load "tests/1-1.scm")
+(load "tests/1-2.scm")
 
 (define (emit-program expr)
-  (define (emit-prim x)
-    (cond
-      (integer? x) (emit "ret i32 ~a" x)))
-  
-  (emit "define dso_local i32 @scheme_entry() #0 {")
-  (emit "ret i32 ~a" expr)
+  (define expr-type (get-type expr))
+
+  (emit "define dso_local ~a @scheme_entry() #0 {" expr-type)
+  (emit "ret ~a ~b" expr-type (number->string (boxed-rep expr)))
   (emit "}"))
+  
+(define (immediate-rep x)
+  (cond
+    [(integer? x) (number->string x)]
+    [(null? x) 47] ;; 00101111
+    [(char? x) (char->integer x)]
+    [(boolean? x) (if x 1 0)]
+    [else x]))
+  
+(define (get-type x)
+  (cond
+    [(integer? x) "i32"]
+    [(null? x) "i32"]
+    [(char? x) "i32"]
+    [(boolean? x) "i32"]
+    [else "i8"]))
+
+(define fixnum-shift 2)
+
+(define char-tag 15)
+(define char-shift 8)
+
+(define bool-tag 31)
+(define bool-shift 8)
+
+(define (boxed-rep x)
+  (cond
+    [(integer? x) (ash x fixnum-shift)]
+    [(null? x) 47] ;; 00101111
+    [(boolean? x) (let ([val (if x 1 0)])
+                       (logior (ash val bool-shift) bool-tag))]
+    [(char? x) (let ([val (char->integer x)])
+                    (logior (ash val char-shift) char-tag))]
+    [else 0]))
+
+(define (number->binary n)
+  (if (= n 0)
+    0
+    (+ (mod n 2)
+      (* 10 (number->binary (quotient n 2))))))
--- src/runtime.c
+++ src/runtime.c
@@ -1,6 +1,45 @@
 /* a simple drive for scheme_entry */
 #include <stdio.h>
+
+#define fixnum_mask    3
+#define fixnum_tag     0
+#define fixnum_shift   2
+
+#define boolean_mask   255
+#define boolean_tag    31
+#define boolean_shift  8
+
+#define char_mask      255
+#define char_tag       15
+#define char_shift     8
+
+#define empty_list     47
+
 int main(int argc, char** argv){
-  printf("%d\n", scheme_entry());
+  int val = scheme_entry();
+
+  if ((val & fixnum_mask) == fixnum_tag){
+    printf("%d\n", val >> fixnum_shift);
+  } else if ((val & boolean_mask) == boolean_tag){
+    int boolval = val >> boolean_shift;
+    if (boolval) {
+      printf("#t\n");
+    } else {
+      printf("#f\n");
+    }
+  } else if (val == empty_list){
+    printf("()\n");
+  } else if ((val & char_mask) == char_tag){
+    int c = val >> char_shift;
+
+    if      (c == '\t') { printf("#\\tab\n"); }
+    else if (c == '\n') { printf("#\\newline\n"); }
+    else if (c == '\r') { printf("#\\return\n"); }
+    else if (c == ' ')  { printf("#\\space\n"); }
+    else                { printf("#\\%c\n", c); }
+  } else {
+    printf("unrecognized value: %d\n", val);
+  }
+
   return 0;
 }
